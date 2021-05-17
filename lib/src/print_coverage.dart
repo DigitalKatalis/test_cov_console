9bdec09be7ebc79c0c9e2cec49e3e6dbe0b64eb9
I Made Mudita <made.mudita@mitrais.com>
2021-05-17 16:49:07 +0800

    Replace I Made Mudita with DKatalis

--- lib/src/print_coverage.dart
+++ lib/src/print_coverage.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2021, I Made Mudita. All rights reserved. Use of this source code
+// Copyright (c) 2021, DKatalis. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'dart:io';
