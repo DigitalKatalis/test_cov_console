9aa6fce35d576fdd253ec46e59cceccc5c39af46
I Made Mudita <made.mudita@tech.jago.com>
2021-10-10 18:16:04 +0800

    Support for multiple lcov.info files.

--- bin/test_cov_console.dart
+++ bin/test_cov_console.dart
@@ -16,6 +16,7 @@ import 'package:test_cov_console/test_cov_console.dart';
 /// -e, --exclude=<STRING1,STRING2,...>
 ///                           a list of contains string for files without unit testing
 ///                           to be excluded from report
+/// -m, --multi               report from multiple lcov.info files
 /// -h, --help                show this help
 /// ```
 Future main(List<String> arguments) async {
@@ -30,10 +31,28 @@ Future main(List<String> arguments) async {
   final slash = Platform.isWindows ? '\\' : '/';
   String lcovFile = args[ParserConstants.file] ?? 'coverage${slash}lcov.info';
 
+  if (args[ParserConstants.multi] == ParserConstants.multi) {
+    final files = await getLCov('./', replaceSlash(lcovFile));
+    for (final file in files) {
+      final dir = file
+          .toString()
+          .replaceAll(replaceSlash(lcovFile), '')
+          .replaceAll('/', '');
+      final lCovFullPath = '${dir.isEmpty ? '' : '$dir$slash'}$lcovFile';
+      final libFullPath = '${dir.isEmpty ? '' : '$dir$slash'}lib';
+      await _printSingleLCov(lCovFullPath, patterns, libFullPath, ' - $dir -');
+    }
+  } else {
+    await _printSingleLCov(lcovFile, patterns, 'lib', '');
+  }
+}
+
+Future<void> _printSingleLCov(
+    String lcovFile, List<String> patterns, String lib, String module) async {
   List<String> lines = await File(lcovFile).readAsLines();
   if (Platform.isWindows) {
     lines = lines.map((line) => replaceSlash(line)).toList();
   }
-  final files = await getFiles('lib', patterns);
-  printCov(lines, files);
+  final files = await getFiles(lib, patterns);
+  printCov(lines, files, module);
 }
