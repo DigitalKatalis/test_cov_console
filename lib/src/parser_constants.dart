6f1559c701f0e4434e55184c0a9e4ae75b6a5ab4
I Made Mudita <made.mudita@tech.jago.com>
2021-10-12 08:31:13 +0800

    Add output format option

--- lib/src/parser_constants.dart
+++ lib/src/parser_constants.dart
@@ -3,6 +3,7 @@ class ParserConstants {
   static const file = 'file';
   static const exclude = 'exclude';
   static const multi = 'multi';
+  static const csv = 'csv';
   static const String invalid = 'Error invalid parameter!\n';
   static const String helpPrint =
       'Generate coverage test report from lcov.info file to console.\n'
@@ -11,12 +12,14 @@ class ParserConstants {
       '-e, --exclude=<STRING1,STRING2,...>    a list of contains string for files without unit testing\n'
       '                                       to be excluded from report\n'
       '-m, --multi                            report from multiple lcov.info files\n'
+      '-c, --csv                              output to CSV file'
       '-h, --help                             show this help';
 
   static List<Option> fieldMap = [
     Option('f', file, false, false),
     Option('e', exclude, true, false),
     Option('m', multi, false, true),
+    Option('c', csv, false, true),
   ];
 }
 
