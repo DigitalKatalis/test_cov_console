d7d03e56b7dfd360fdb026c38c1cb6eaabe521e7
I Made Mudita <84303702+dk-imudita@users.noreply.github.com>
2021-10-25 08:15:39 +0800

    Dev (#16)
    * Remove flutter_test lib
    
    * Increase version
    
    Co-authored-by: I Made Mudita <made.mudita@tech.jago.com>

--- test/test_cov_console_test.dart
+++ test/test_cov_console_test.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 import 'package:test_cov_console/test_cov_console.dart';
 
 var log = <String>[];
