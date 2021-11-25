f6359e290d02f0dc2a79ae4b4002e4b6162ff308
Elsabe Ros <ElsabeR@discovery.co.za>
2021-11-25 09:50:37 +0200

    More fixes.

--- lib/src/print_cov.dart
+++ lib/src/print_cov.dart
@@ -214,26 +214,6 @@ void _printTotal(bool isCsv, bool isLineOnly, _Data totalData) {
 ///
 /// It will print header coverage lines
 void _printHeader(bool isCsv, bool isLineOnly, String module) {
-  _print(
-    PrintCovConstants.dash,
-    PrintCovConstants.dash,
-    PrintCovConstants.dash,
-    PrintCovConstants.dash,
-    PrintCovConstants.dash,
-    PrintCovConstants.dash,
-    isCsv,
-    isLineOnly,
-  );
-  _print(
-      "**" + "${PrintCovConstants.file}$module" + "**",
-      "**" + PrintCovConstants.branch + "**",
-      "**" + PrintCovConstants.functions + "**",
-      "**" + PrintCovConstants.lines + "**",
-      "**" + PrintCovConstants.unCovered + "**",
-      PrintCovConstants.space,
-      isCsv,
-      isLineOnly,
-      isSave: true);
   // _print(
   //   PrintCovConstants.dash,
   //   PrintCovConstants.dash,
@@ -244,6 +224,26 @@ void _printHeader(bool isCsv, bool isLineOnly, String module) {
   //   isCsv,
   //   isLineOnly,
   // );
+  _print(
+      "**" + "${PrintCovConstants.file}$module" + "**",
+      "**" + PrintCovConstants.branch + "**",
+      "**" + PrintCovConstants.functions + "**",
+      "**" + PrintCovConstants.lines + "**",
+      "**" + PrintCovConstants.unCovered + "**",
+      PrintCovConstants.space,
+      isCsv,
+      isLineOnly,
+      isSave: true);
+  _print(
+    PrintCovConstants.dash,
+    PrintCovConstants.dash,
+    PrintCovConstants.dash,
+    PrintCovConstants.dash,
+    PrintCovConstants.dash,
+    PrintCovConstants.dash,
+    isCsv,
+    isLineOnly,
+  );
 }
 
 /// _getCoverage
